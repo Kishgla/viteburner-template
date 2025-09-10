7a4ca53180b3789a1ab010bdceff28d21899f6ff
Michael Duffy <Kishgla@gmail.com>
2025-09-10 16:46:17 -0700

    Add gang manager to automate.js

--- src/automate.js
+++ src/automate.js
@@ -3,4 +3,5 @@ export async function main(ns) {
   ns.exec("hacknet-automator.js", "home");
   ns.exec("dynamic-hack-scheduler.js", "home");
   ns.exec("purchase-servers.js", "home");
+  ns.exec("gang-manager.js", "home");
 }
