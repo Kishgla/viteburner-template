e570fa331f46dbab15540858d03996be262d93c6
Michael Duffy <Kishgla@gmail.com>
2025-09-22 18:05:52 -0700

    Add stock trader to Automate.js

--- src/automate.js
+++ src/automate.js
@@ -4,4 +4,5 @@ export async function main(ns) {
   ns.exec("dynamic-hack-scheduler.js", "home");
   ns.exec("purchase-servers.js", "home");
   ns.exec("gang-manager.js", "home");
+  ns.exec("Stock-trader.js", "home");
 }
